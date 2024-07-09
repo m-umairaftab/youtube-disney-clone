cf16859fd95ab50836e7371a83528a8f9a455791
m-umairaftab <m.umair.umairaftab@gmail.com>
2024-07-09 18:08:41 +0500

    youtube disney clone

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          {/* <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
